78621b17810b10ab989b1e93359bfe1d05ecf222
Zhaniya <120751675+zhanikowi@users.noreply.github.com>
2022-12-20 12:58:42 +0600

    s
    s

--- home.js
+++ home.js
@@ -1,11 +1,3 @@
-$(document).ready(function(){
-    $("imgintro1").mouseenter(function(){
-    $("img").animate();
-    });
-    $("#show").click(function(){
-    $("img").show();
-    });
-});
 
 $(document).ready(function(){
     $("#gamee").click(function(){
